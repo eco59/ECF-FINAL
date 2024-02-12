956c1a9d284eb155f92d48654e2647d93e8e683d
eco59 <137323817+eco59@users.noreply.github.com>
2024-02-12 15:04:27 +0100

    Update logout.php

--- web/deconnexion/logout.php
+++ web/deconnexion/logout.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <?php
     session_start();
     session_unset();
     session_destroy();
     header('Location: ../../index.php');
     exit();
-=======
-<?php
-    session_start();
-    session_unset();
-    session_destroy();
-    header('Location: ../../index.php');
-    exit();
->>>>>>> a02418468dad454e9d1471734a741ca2dc502094
 ?>
